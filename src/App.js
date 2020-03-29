47ad44bc400e29a1cd14455ecffcdd80df0c145d
wbmmusic <wbmmusic@gmail.com>
2020-03-29 18:20:30 -0500

    Version 0.0.17 update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Components/layout/header'
 import Todos from './Components/Todos'
